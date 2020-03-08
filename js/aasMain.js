cc97aaf00e9c850744bbb26d377a2100820c7efe
Jonathan Yang <jonathan@JonathandeMacBook-Pro.local>
2020-03-08 11:41:58 +0800

    adjust the navbar etc

--- js/aasMain.js
+++ js/aasMain.js
@@ -14,6 +14,24 @@ $(function () {
 //     $('html,body').animate({scrollTop:0},'slow');return false;
 // });  
 
-$( '#backToTopBtn' ).on( 'click', function () {
+// 定义#backToTopBtn点击的事件
+$('#backToTopBtn').on( 'click', function () {
   $('html,body').animate({scrollTop:0},'slow');return false;
+});
+
+// 定义#backToTopBtn出现的时机
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 80) {
+    $('#backToTopBtn').fadeIn('slow');
+  } else {
+    $('#backToTopBtn').fadeOut('slow');
+  }
+});
+
+// 手机设备时候，点击收回菜单，需要定义多个需要的对象id
+// 改为用css来标记
+// $('.navbar-nav li a ).on('click',function () {
+$('.navbar-nav li  .navMenuItem').on('click',function () {
+  // $('#mHome, #mProd, #mContact').on('click',function () {
+  $('#aasNavbars').collapse('hide');
 });
--- js/aasNavBar.js
+++ js/aasNavBar.js
@@ -1,4 +1,4 @@
 $( '#aasNavbars .navbar-nav a' ).on( 'click', function () {
-	$( '#aasNavbars .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-	$( this ).parent( 'li' ).addClass( 'active' );
+	// $( '#aasNavbars .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+	// $( this ).parent( 'li' ).addClass( 'active' );
 });
